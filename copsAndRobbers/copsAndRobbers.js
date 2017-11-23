71765a2c21f1a9f52e9555af480cac7792c49190
Anshul Chandra <canshul0803@gmail.com>
2017-11-23 17:58:20 -0500

    Added scoring

--- copsAndRobbers/copsAndRobbers.js
+++ copsAndRobbers/copsAndRobbers.js
@@ -15,15 +15,15 @@ CopsAndRobbersGame.prototype.init = function () {
   this.levels = new Levels(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["wall"]);
   this.levels.addLevelObjectsToEngine(this.levels.current_level);
 
-  this.createGraph(levelWidth, levelHeight,this.levels.nodeArray);
+  this.createGraph(levelWidth, levelHeight, this.levels.nodeArray);
   // Initialize scoring
-  //this.init_scoring();
+  this.init_scoring();
 
   this.players = [];
-  this.players[0] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["robber2"],"robber", 0, 10, true);
-  this.players[1] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["cop"],"cop", 1, 40, true);
-  this.players[2] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["robber2"],"robber", 2, 70, false);
-  this.players[3] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["cop"],"cop", 3, 16, true);
+  this.players[0] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["robber2"], "robber", 0, 10, true);
+  this.players[1] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["cop"], "cop", 1, 40, true);
+  this.players[2] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["robber2"], "robber", 2, 70, false);
+  this.players[3] = new Player(this.engine, levelWidth, levelHeight, this.cellSize, this.spriteStyle["cop"], "cop", 3, 16, true);
   this.turn = 0;
   this.closestEnemy = 0;
   this.engine.input.setObjectSelected(this.players[0].sprite.id);
@@ -34,6 +34,25 @@ CopsAndRobbersGame.prototype.init = function () {
   this.engine.setUpdateHandler(this.update.bind(this));
   this.engine.setDrawHandler(this.draw.bind(this));
   this.engine.graph.setHeuristic(this.heuristic.bind(this));
+}
+
+CopsAndRobbersGame.prototype.init_scoring = function(){
+  // Score is equivalent to number of turns taken by cops
+  this.score = 0.0;
+  this.maxTurns = 16;
+
+  this.scoreLable = document.getElementById('turns');
+  document.getElementById('maxTurns').innerText = this.maxTurns;
+  this.update_score();
+}
+
+CopsAndRobbersGame.prototype.update_score = function(){
+  var turns = Math.floor(this.score);
+  this.scoreLable.innerText = turns;
+
+  if(turns >= this.maxTurns){
+    this.endGame("Robbers win!");
+  }
 }
 
 function Player(engine, width, height, cellSize, spriteStyle, label, id, nodeId, isAI) {
@@ -50,24 +69,20 @@ function Player(engine, width, height, cellSize, spriteStyle, label, id, nodeId,
   this.create_player(height, width, cellSize, nodeId);
 }
 
-Player.prototype.create_player = function(height,width,cellSize)
-{
-    var x = this.currentNodeId%width * cellSize;
-    var y = Math.floor(this.currentNodeId/height) * cellSize;
-    this.sprite = new Sprite(x, y, cellSize, cellSize, this.spriteStyle);
-    this.sprite.tags.label = this.label;
-    this.sprite.tags.id = this.id;
-    this.engine.addObject(this.sprite);
+Player.prototype.create_player = function (height, width, cellSize) {
+  var x = this.currentNodeId % width * cellSize;
+  var y = Math.floor(this.currentNodeId / height) * cellSize;
+  this.sprite = new Sprite(x, y, cellSize, cellSize, this.spriteStyle);
+  this.sprite.tags.label = this.label;
+  this.sprite.tags.id = this.id;
+  this.engine.addObject(this.sprite);
 }
 
-Player.prototype.move= function(newNodeId)
-{
-  for(var index = 0; index < this.engine.graph.edges.length ; index++)
-  {
-    if(this.engine.graph.edges[index].source == this.currentNodeId && this.engine.graph.edges[index].goal == newNodeId)
-    {
-      this.sprite.X =  newNodeId%this.width * this.cellSize;
-      this.sprite.Y =  Math.floor(newNodeId/this.height) * this.cellSize;
+Player.prototype.move = function (newNodeId) {
+  for (var index = 0; index < this.engine.graph.edges.length; index++) {
+    if (this.engine.graph.edges[index].source == this.currentNodeId && this.engine.graph.edges[index].goal == newNodeId) {
+      this.sprite.X = newNodeId % this.width * this.cellSize;
+      this.sprite.Y = Math.floor(newNodeId / this.height) * this.cellSize;
       this.currentNodeId = newNodeId;
       this.engine.input.setMovedObject(this.sprite.id);
       return true;
@@ -77,92 +92,86 @@ Player.prototype.move= function(newNodeId)
   return false;
 }
 
-CopsAndRobbersGame.prototype.createGraph = function(levelWidth, levelHeight, nodeArray)
-{
-  for(var index = 0; index < nodeArray.length; index++)
-  {
+CopsAndRobbersGame.prototype.createGraph = function (levelWidth, levelHeight, nodeArray) {
+  for (var index = 0; index < nodeArray.length; index++) {
     // console.log(nodeArray[index]%levelWidth);
-     this.engine.graph.addNode(nodeArray[index]%levelWidth, Math.floor(nodeArray[index]/levelHeight), nodeArray[index]);   
+    this.engine.graph.addNode(nodeArray[index] % levelWidth, Math.floor(nodeArray[index] / levelHeight), nodeArray[index]);
   }
 
-  for(var i = 0; i < nodeArray.length; i++)
-  {
-    for(var j = 0; j < nodeArray.length; j++)
-    {
-      if(i!=j)
-      {
-        if(nodeArray[i]+levelWidth == nodeArray[j] || nodeArray[i]-levelWidth == nodeArray[j])
-        {
+  for (var i = 0; i < nodeArray.length; i++) {
+    for (var j = 0; j < nodeArray.length; j++) {
+      if (i != j) {
+        if (nodeArray[i] + levelWidth == nodeArray[j] || nodeArray[i] - levelWidth == nodeArray[j]) {
           this.engine.graph.addEdge(nodeArray[i], nodeArray[j], 1);
         }
-        if(nodeArray[i]+1 == nodeArray[j] || nodeArray[i]-1 == nodeArray[j])
-        {
+        if (nodeArray[i] + 1 == nodeArray[j] || nodeArray[i] - 1 == nodeArray[j]) {
           this.engine.graph.addEdge(nodeArray[i], nodeArray[j], 1);
         }
       }
-    } 
+    }
   }
 
 }
 
 CopsAndRobbersGame.prototype.restart = function () {
-  this.pauseGame = false;
+  window.location.reload(true);
 }
 
-CopsAndRobbersGame.prototype.endGame = function () {
-  alert("Cop Wins!");
+CopsAndRobbersGame.prototype.endGame = function (message) {
+  alert(message);
+  window.location.reload(true);
   this.pauseGame = true;
 }
 
 CopsAndRobbersGame.prototype.handleCollission = function (head, collidedSprite) {
   var cop = null;
   var robber = null;
-  if(head.tags.label == "cop" && collidedSprite.tags.label == "robber") {
+  if (head.tags.label == "cop" && collidedSprite.tags.label == "robber") {
     cop = head;
     robber = collidedSprite;
   }
-  if(head.tags.label == "robber" && collidedSprite.tags.label == "cop") {
+  if (head.tags.label == "robber" && collidedSprite.tags.label == "cop") {
     cop = collidedSprite;
     robber = head;
   }
-  if(cop != null && robber != null) {
+  if (cop != null && robber != null) {
     console.log("Deleting robber")
     this.engine.deleteObject(robber.id);
     //this.players.splice(robber.tags.id, 1);
     var robberObj;
 
-    for(var i = 0; i < this.players.length; i++) {
-      if(this.players[i].sprite.id == robber.id) {
+    for (var i = 0; i < this.players.length; i++) {
+      if (this.players[i].sprite.id == robber.id) {
         robberObj = this.players[i];
         this.players[i].isDead = true;
       }
     }
     //this.turn = this.turn % this.players.length;
     var foundNewEnemy = false;
-    for(var i = 0; i < this.players.length; i++) {
-      if(this.players[i].label == "robber" && this.players[i].isDead == false) {
+    for (var i = 0; i < this.players.length; i++) {
+      if (this.players[i].label == "robber" && this.players[i].isDead == false) {
         this.closestEnemy = i;
         foundNewEnemy = true;
       }
     }
 
-    if(foundNewEnemy == false)
-      this.endGame();
+    if (foundNewEnemy == false)
+      this.endGame("Cops win!");
 
-    if(this.turn == robberObj.id)
+    if (this.turn == robberObj.id)
       this.updateTurnIfDead();
 
   }
 }
 
 
-CopsAndRobbersGame.prototype.getNodeId = function(direction, currentNodeId) {
+CopsAndRobbersGame.prototype.getNodeId = function (direction, currentNodeId) {
   var newNodeId;
-  if(direction == "ArrowLeft")
+  if (direction == "ArrowLeft")
     newNodeId = currentNodeId - 1;
-  else if(direction == "ArrowRight")
+  else if (direction == "ArrowRight")
     newNodeId = currentNodeId + 1;
-  else if(direction == "ArrowUp")
+  else if (direction == "ArrowUp")
     newNodeId = currentNodeId - this.levelWidth;
   else
     newNodeId = currentNodeId + this.levelWidth;
@@ -175,27 +184,26 @@ CopsAndRobbersGame.prototype.keyPressed = function (key) {
     case "ArrowRight":
     case "ArrowUp":
     case "ArrowDown":
-        var newNodeId = this.getNodeId(key, this.players[this.turn].currentNodeId);
-        if(this.players[this.turn].isAI == false && this.players[this.turn].isDead == false) {
-          if(this.players[this.turn].move(newNodeId) == true) 
-            {
-              this.updateTurnIfDead();
-            }
+      var newNodeId = this.getNodeId(key, this.players[this.turn].currentNodeId);
+      if (this.players[this.turn].isAI == false && this.players[this.turn].isDead == false) {
+        if (this.players[this.turn].move(newNodeId) == true) {
+          this.updateTurnIfDead();
         }
+      }
       break;
-    
+
     case "KeyR":
       this.restart();
   }
 }
 
 CopsAndRobbersGame.prototype.heuristic = function (source, goal) {
- //return 0;
-  var sx = source%this.levelWidth;
-  var gx = goal%this.levelWidth;
-  var sy = Math.floor(source/this.levelWidth);
-  var gy = Math.floor(goal/this.levelWidth);
-  return (sx-gx)*(sx-gx) + (sy-gy)*(sy-gy);
+  //return 0;
+  var sx = source % this.levelWidth;
+  var gx = goal % this.levelWidth;
+  var sy = Math.floor(source / this.levelWidth);
+  var gy = Math.floor(goal / this.levelWidth);
+  return (sx - gx) * (sx - gx) + (sy - gy) * (sy - gy);
 }
 
 CopsAndRobbersGame.prototype.loadContent = function () {
@@ -213,9 +221,9 @@ CopsAndRobbersGame.prototype.loadContent = function () {
 }
 
 
-CopsAndRobbersGame.prototype.updateTurnIfDead = function() {
+CopsAndRobbersGame.prototype.updateTurnIfDead = function () {
   this.turn = (this.turn + 1) % this.players.length;
-  while(this.players[this.turn].isDead == true)
+  while (this.players[this.turn].isDead == true)
     this.turn = (this.turn + 1) % this.players.length;
   this.engine.input.setObjectSelected(this.players[this.turn].sprite.id);
 }
@@ -227,23 +235,27 @@ CopsAndRobbersGame.prototype.update = function () {
 
   this.engine.update();
 
-  
+
   if (this.players[this.turn].isAI == true) {
     var newNode = null;
-    if(this.players[this.turn].label == 'robber'){
-      var directions = ["ArrowLeft", "ArrowRight", "ArrowUp","ArrowDown"];
-      var direction = Math.floor(Math.random()*directions.length);
-      newNode = this.getNodeId(directions[direction],this.players[this.turn].currentNodeId);
+    if (this.players[this.turn].label == 'robber') {
+      var directions = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
+      var direction = Math.floor(Math.random() * directions.length);
+      newNode = this.getNodeId(directions[direction], this.players[this.turn].currentNodeId);
     }
-    else{
+    else {
       var closestEnemy = this.closestEnemy; //this.players[this.turn].closestEnemy;
       console.log("Running astar for " + this.players[this.turn].currentNodeId, this.players[closestEnemy].currentNodeId);
       var path = this.engine.pathSearch.astar(this.engine.graph, this.players[this.turn].currentNodeId, this.players[closestEnemy].currentNodeId);
       newNode = path[path.length - 2];
+
+      this.score += 0.5;
     }
-    if(this.players[this.turn].move(newNode)) {
-        this.updateTurnIfDead();
+    if (this.players[this.turn].move(newNode)) {
+      this.updateTurnIfDead();
     }
+
+    this.update_score();
   }
 }
 
@@ -300,32 +312,28 @@ Levels.prototype.create_levels = function (height, width, cellSize) {
 
   //Level 0
   this.gameLevels[0] = new Array();
-  
+
   var name = "wall";
-  this.nodeArray = [10,15,16,19,20,21,22,24,28,31,33,37,38,39,40,41,42,43,46,48,52, 55, 57,61,64,66,67,68,69,70];
+  this.nodeArray = [10, 15, 16, 19, 20, 21, 22, 24, 28, 31, 33, 37, 38, 39, 40, 41, 42, 43, 46, 48, 52, 55, 57, 61, 64, 66, 67, 68, 69, 70];
   var wallArray = [];
   var current_pointer = 0;
-  for(var i = 0; i < width*height; i++)
-  {
-     if(i != this.nodeArray[current_pointer])
-     {  
-        wallArray.push(i);
-     }
-     else
-     {
-        current_pointer++;
-     }
+  for (var i = 0; i < width * height; i++) {
+    if (i != this.nodeArray[current_pointer]) {
+      wallArray.push(i);
+    }
+    else {
+      current_pointer++;
+    }
   }
 
-  for(var i = 0; i < wallArray.length ; i++)
-  {
-      var x = (wallArray[i] % width ) * cellSize;
-      var y = (Math.floor(wallArray[i] / height)) * cellSize;
-      var current_sprite = new Sprite(x, y, cellSize, cellSize, this.spriteStyle);
-      current_sprite.tags.name = name;
-      this.gameLevels[0].push(current_sprite);  
+  for (var i = 0; i < wallArray.length; i++) {
+    var x = (wallArray[i] % width) * cellSize;
+    var y = (Math.floor(wallArray[i] / height)) * cellSize;
+    var current_sprite = new Sprite(x, y, cellSize, cellSize, this.spriteStyle);
+    current_sprite.tags.name = name;
+    this.gameLevels[0].push(current_sprite);
   }
- 
+
 }
 
 
@@ -343,7 +351,7 @@ Levels.prototype.removeLevelObjectsFromEngine = function (level) {
 }
 
 Levels.prototype.update = function (score) {
-  
+
 }
 
 
